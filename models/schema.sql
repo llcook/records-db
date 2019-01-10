30a7d8ffbecd2a1f7c3007f429f822d270d02f59
dewyoyo <dewyoyo@hotmail.com>
2019-01-10 11:53:26 -0700

    documents model upload

--- models/schema.sql
+++ models/schema.sql
@@ -1,4 +1,18 @@
--- Drops the blogger if it exists currently --
+-- Drops the  db if it exists currently --
 DROP DATABASE IF EXISTS records_db;
--- Creates the "blogger" database --
+-- Creates the "db" database --
 CREATE DATABASE records_db;
+USE records_db;
+CREATE TABLE document
+(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    source VARCHAR(100),
+    document_name VARCHAR(100),
+    docdate DATETIME,
+    category VARCHAR(100),
+    description VARCHAR(300),
+    image BLOB,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+)
---
+++ models/seeds.sql
@@ -0,0 +1,24 @@
+-- write insert queries to populate the `burgers` table with at least three entries.
+
+-- id INTEGER NOT NULL AUTO_INCREMENT,
+--     source VARCHAR(100),
+--     document_name VARCHAR(100),
+--     docdate DATETIME,
+--     category VARCHAR(100),
+--     description VARCHAR(300),
+--     image BLOB,
+--     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+--     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+--     PRIMARY KEY (id)
+
+
+
+
+INSERT INTO document (source,document_name, docdate, category , description, image, createdAt, updatedAt) 
+VALUES ('file:///F:/test-folder/table-sort.3/index.html#', 'test01', CURRENT_TIMESTAMP, 'politics', 'test document data1', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO document (source,document_name, docdate, category , description, image, createdAt, updatedAt)
+VALUES ('file:///F:/test-folder/table-sort.3/index.html#', 'test02', CURRENT_TIMESTAMP, 'politics', 'test document data2', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO document (source,document_name, docdate, category , description, image, createdAt, updatedAt)
+VALUES ('file:///F:/test-folder/table-sort.3/index.html#', 'test03', CURRENT_TIMESTAMP, 'personal', 'test document data3', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP); 
+INSERT INTO document (source,document_name, docdate, category , description, image, createdAt, updatedAt)
+VALUES ('file:///F:/test-folder/table-sort.3/index.html#', 'test04', CURRENT_TIMESTAMP, 'personal', 'test document data4', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
