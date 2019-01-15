709b35967bfc2219247203bf2900cdb37c873976
llcook <29869241+llcook@users.noreply.github.com>
2019-01-15 09:25:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ require("./routes/htmlRoutes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
